b9d8344e10a57000049ae20a8bff9902304735a8
czubajka <czubajka.kania@gmail.com>
2018-08-13 23:32:55 +0200

    add main.cpp with introduction

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This in RExpediter v.0.1" << std::endl;
+	std::cout << "made by czubajka kania" << std::endl;
+	std::cout << "\nPress any key to start:" << std::endl;
+	std::cin.get();
+}
